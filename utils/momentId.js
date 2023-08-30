7d31da6965bdc1e26a75f0afed20e9d1fc954403
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-30 14:45:28 +0700

    fix: Validasi View CV Submit Kandidat

--- utils/momentId.js
+++ utils/momentId.js
@@ -3,9 +3,9 @@ import "moment/locale/id";
 moment.locale("id");
 
 export const MomentDateID = (field) => {
-  return moment(field).utc(0).format("DD MMM YYYY");
+  return moment(field).utc(1).format("DD MMM YYYY");
 };
 
 export const MomentTimeID = (field) => {
-  return moment(field).utc(0).format("HH:mm:ss");
+  return moment(field).utc(1).format("HH:mm:ss");
 };
